b603bdb8c9011b603f564ed2531e01fdf54185b9
John Metzger <56820659+jometzg@users.noreply.github.com>
2025-05-30 11:45:54 +0100

    Create main.tf

---
+++ iac/azure/level4/storage/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.29.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "zerotrust-rg"
+  location = "UK South"
+}
+
+resource "azurerm_user_assigned_identity" "uami" {
+  name                = "uami-container-access"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "jjblobstorageacct01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "blob_reader" {
+  scope                = azurerm_storage_account.storage.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_user_assigned_identity.uami.principal_id
+}
